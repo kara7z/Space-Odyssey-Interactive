5743d7e444f2aaa30402924fc3e61dd64b26d3ee
kara7z <kara.ossama12@gmail.com>
2025-10-30 12:45:03 +0100

    Adding the JavaScript File

---
+++ index.js
@@ -0,0 +1,124 @@
+let data = [
+    {
+        "id": 1,
+        "name": "Apollo 11",
+        "agency": "NASA",
+        "objective": "Achieve the first crewed landing on the Moon.",
+        "launchDate": "1969-07-16",
+        "image": "IMAGES/appollo.jpg",
+        "description": "Apollo 11 was the first mission to land humans on the Moon. Neil Armstrong and Buzz Aldrin walked its surface, marking a giant leap for mankind."
+    },
+    {
+        "id": 2,
+        "name": "Voyager 1",
+        "agency": "NASA",
+        "objective": "Explore the outer Solar System and interstellar space.",
+        "launchDate": "1977-09-05",
+        "image": "IMAGES/Voyager 1.jpg",
+        "description": "Voyager 1 is the farthest human-made object from Earth. It explored the outer planets and now travels through interstellar space, carrying a message from humanity."
+
+    },
+    {
+        "id": 3,
+        "name": "Hubble Space Telescope (HST)",
+        "agency": "NASA / ESA",
+        "objective": "Conduct high-resolution astronomy from Earth's orbit.",
+        "launchDate": "1990-04-24",
+        "image": "IMAGES/3. Hubble Space Telescope (HST).jpg",
+        "description": "The Hubble Space Telescope orbits Earth, capturing stunning images of distant galaxies and nebulae. Its discoveries have deepened our understanding of the universe."
+    },
+    {
+        "id": 4,
+        "name": "Cassini–Huygens",
+        "agency": "NASA / ESA / ASI",
+        "objective": "Study Saturn, its rings, and its moons.",
+        "launchDate": "1997-10-15",
+        "image": "IMAGES/4. Cassini–Huygens.jpg",
+        "description": "Cassini Huygens explored Saturn and its moons, revealing their beauty and complexity. The mission uncovered active moons and stunning rings before its final dive into the planet."
+    },
+    {
+        "id": 5,
+        "name": "Juno",
+        "agency": "NASA",
+        "objective": "Investigate Jupiter's origin and interior structure.",
+        "launchDate": "2011-08-05",
+        "image": "IMAGES/Juno.jpg",
+        "description": "Juno orbits Jupiter to study its atmosphere, magnetic field, and deep structure. Its mission helps reveal how the giant planet — and our solar system — formed."
+    },
+    {
+        "id": 6,
+        "name": "Curiosity (MSL)",
+        "agency": "NASA",
+        "objective": "Search for evidence of past life conditions on Mars.",
+        "launchDate": "2011-11-26",
+        "image": "IMAGES/Curiosity.jpg",
+        "description": "Curiosity explores the surface of Mars to study its climate, geology, and potential for past life. Its discoveries continue to shape our understanding of the Red Planet."
+    },
+    {
+        "id": 7,
+        "name": "James Webb Space Telescope (JWST)",
+        "agency": "NASA / ESA / CSA",
+        "objective": "Observe the first galaxies and exoplanets..",
+        "launchDate": "2021-12-25",
+        "image": "IMAGES/7. James Webb Space Telescope (JWST).jpg",
+        "description": "The James Webb Space Telescope peers deep into space to observe the first galaxies and stars. Its advanced instruments reveal the origins and evolution of the universe."
+    },
+    {
+        "id": 8,
+        "name": "Perseverance (Mars 2020)",
+        "agency": "NASA",
+        "objective": "Collect rock samples on Mars for return to Earth.",
+        "launchDate": "2020-07-30",
+        "image": "IMAGES/9. Perseverance (Mars 2020).jpg",
+        "description": "Perseverance explores Mars to search for signs of past life and collect samples. Its mission helps pave the way for future human exploration of the Red Planet."
+    },
+    {
+        "id": 9,
+        "name": "Lucy",
+        "objective": "Explore the Trojan asteroids near Jupiter.",
+        "launchDate": "2021-10-16",
+        "image": "IMAGES/Lucy.webp",
+        "agency": "NASA",
+        "description": "Lucy is studying the Trojan asteroids that orbit near Jupiter. Its mission aims to understand the building blocks of the planets and the early history of our solar system."
+    },
+    {
+        "id": 10,
+        "name": "Artemis I",
+        "agency": "NASA",
+        "objective": "Uncrewed test flight of the new Orion spacecraft to the Moon.",
+        "launchDate": "2022-11-16",
+        "image": "IMAGES/8. Artemis I.jpg",
+        "description": "Artemis I is an uncrewed mission testing the Space Launch System and Orion spacecraft. It paves the way for future human exploration of the Moon and beyond."
+    }
+]
+for (let i = 0; i < data.length; i++) {
+    document.getElementById("fist-row").innerHTML += `<div class="container">
+        <span class="missionstilte">
+            <h1>${data[i].name}</h1>
+            <hr id="UNDRELINE">
+        </span>
+        <div class="contt">
+            <div class="mission-img">
+                <img src="${data[i].image}">
+            </div>
+            <div class="d-text">
+                <div class="Box-Details">
+                    <h1>DETAILS:</h1>
+                    <p>Agency: ${data[i].agency}</p>
+                    <p>Launch Date: ${data[i].launchDate}</p>
+                    <p>Objective: ${data[i].objective}</p>
+
+                </div>
+                <div class="Box-Description">
+                    <h1>DESCRIPTION:</h1>
+                    <p>${data[i].description}</p>
+                </div>
+            </div>
+        </div>
+        <div class="button-Explore">
+            <button>EXPLORE</button>
+        </div>
+    </div>`;
+}
+
+
